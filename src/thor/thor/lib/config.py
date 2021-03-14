e963ae062f14691e44af484573854e340ed3cb15
Raoni Sian <raonisiann@live.com>
2021-03-14 01:54:02 +0000

    lazy load option added for config files

--- src/thor/thor/lib/config.py
+++ src/thor/thor/lib/config.py
@@ -1,58 +1,55 @@
 import json
 import os
+from thor.lib.base import Base
 
 
 class ConfigException(Exception):
     pass
 
 
-class Config:
-
-    LOADED_CONFIG = {}
+class Config(Base):
 
     def __init__(self, config_path):
+        super().__init__()
         self.__config_path = config_path
-        self.load_config()
+        self.__loaded_config = {}
 
     def load_config_file(self):
-
         if os.path.exists(self.__config_path):
+            self.logger.info('Loading config file %s', self.__config_path)
             with open(self.__config_path) as f:
                 return json.loads(f.read())
         # no config file available
         return None
 
-    def load_config(self):
-
-        if Config.LOADED_CONFIG:
+    def lazy_load_config(self):
+        if self.__loaded_config:
             return
-
         json_config = self.load_config_file()
-
         if json_config:
             self.__load_config_dict_rec(json_config, '')
 
     def __load_config_dict_rec(self, node, base):
-
         for name, value in node.items():
             config_path = '{base}.{name}' if base else '{base}{name}'
             config_path = config_path.format(
                 base=base,
                 name=name
             )
-
             if type(value) is dict:
                 self.__load_config_dict_rec(value, config_path)
             else:
-                Config.LOADED_CONFIG[config_path] = value
+                self.__loaded_config[config_path] = value
 
     def get(self, name):
-        if name in Config.LOADED_CONFIG:
-            return Config.LOADED_CONFIG[name]
+        self.lazy_load_config()
+        if name in self.__loaded_config:
+            return self.__loaded_config[name]
         else:
             return None
 
     def set(self, name, value):
+        self.lazy_load_config()
         if name is not str:
             raise ConfigException('Config name must be an string')
-        Config.LOADED_CONFIG[name] = value
+        self.__loaded_config[name] = value
