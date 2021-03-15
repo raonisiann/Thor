efc8adf99d4c2307d1caa80ab3edef0e1f32b2d3
Raoni Sian <raonisiann@live.com>
2021-03-15 23:19:06 +0000

    new classes for terraform and packer

--- src/thor/thor/lib/terraform.py
+++ src/thor/thor/lib/terraform.py
@@ -2,15 +2,15 @@ from pathlib import Path
 from thor.lib.executable import Executable
 
 
-class PackerException(Exception):
+class TerraformException(Exception):
     pass
 
 
-class Packer(Executable):
+class Terraform(Executable):
 
     def __init__(self):
         super().__init__(
-            'packer',
+            'terraform',
             '{home}/.local/bin'.format(home=Path.home()),
-            'https://releases.hashicorp.com/packer/1.6.5/packer_1.6.5_linux_amd64.zip'
+            'https://releases.hashicorp.com/terraform/0.14.0/terraform_0.14.0_linux_amd64.zip'
         )
