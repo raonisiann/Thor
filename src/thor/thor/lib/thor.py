482b77ab48a482a538aea990195f28c3822d8c44
Raoni Sian <raonisiann@live.com>
2021-04-03 23:00:36 +0000

    implementing a thor class for global stuff

--- src/thor/thor/lib/thor.py
+++ src/thor/thor/lib/thor.py
@@ -1,11 +1,30 @@
 import os
 
-# thor must run from the project root
-#
-# Project root should have environment and
-# image folders.
-ROOT_DIR = os.getcwd()
-BUILD_DIR = f'{ROOT_DIR}/build'
-ENVIRONMENTS_DIR = f'{ROOT_DIR}/environments'
-IMAGES_DIR = f'{ROOT_DIR}/images'
-TEMPLATES_DIR = f'{ROOT_DIR}/templates'
+
+class Thor:
+    # Project root dir
+    ROOT_DIR = os.getcwd()
+    # Main build directory
+    BUILD_DIR = f'{ROOT_DIR}/build'
+    # Environments folder
+    ENVIRONMENTS_DIR = f'{ROOT_DIR}/environments'
+    # Images directory
+    IMAGES_DIR = f'{ROOT_DIR}/images'
+    # Global static files directory
+    STATIC_DIR = f'{ROOT_DIR}/static'
+    # Global templates directory
+    TEMPLATES_DIR = f'{ROOT_DIR}/templates'
+
+    @classmethod
+    def get_template_files(cls):
+        if os.path.exists(cls.TEMPLATES_DIR):
+            return list(os.walk(cls.TEMPLATES_DIR))
+        else:
+            return []
+
+    @classmethod
+    def get_static_files(cls):
+        if os.path.exists(cls.STATIC_DIR):
+            return list(os.walk(cls.STATIC_DIR))
+        else:
+            return []
